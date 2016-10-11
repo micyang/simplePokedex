84685308c38ada9de3af7ad22cdea2952145612a
Michael Yang <michaelyang776@gmail.com>
2016-10-11 16:52:45 -0700

    National Minimal working

--- client/components/national/pokedex.js
+++ client/components/national/pokedex.js
@@ -4,7 +4,7 @@ pokedex.controller('nationalPokedexCtrl', function ($http) {
   vm.status = "Loading...";
   $http({
     method: 'GET',
-    url: 'http://localhost:3000/kanto'
+    url: 'http://localhost:3000/national'
     }).then(function (res) {
       vm.pkmns = res.data;
       vm.status = "";
